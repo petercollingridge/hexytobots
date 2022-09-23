b0e58f36944ded245a2f488f05437f789bca4dcf
Peter Collingridge <peter.collingridge@nanoporetech.com>
2022-09-23 21:31:06 +0100

    Create basic Simulation object

--- js/simulation.js
+++ js/simulation.js
@@ -1,14 +1,34 @@
 const CANVAS_WIDTH = 600;
 const CANVAS_HEIGHT = 400;
+const CELL_SIZE = 10;
 
-const container = document.getElementById('sim');
-const canvas = createElement('canvas')
-  .attr({
-    width: CANVAS_WIDTH,
-    height: CANVAS_HEIGHT
-  })
-  .addTo(container);
-const ctx = canvas.element.getContext('2d');
+const Simulation = function(id) {
+  const container = document.getElementById(id);
+  if (!container) {
+      console.error('No element found with id ' + id);
+      return;
+  }
 
-const cell = new Cell(200, 100, 0.1);
-cell.display(ctx, 10);
+  const canvas = createElement('canvas')
+    .attr({
+      width: CANVAS_WIDTH,
+      height: CANVAS_HEIGHT
+    })
+    .addTo(container);
+
+  this.ctx = canvas.element.getContext('2d');
+
+  this.cells = [
+    new Cell(200, 100, 0.2),
+    new Cell(250, 180, 0),
+  ];
+};
+
+Simulation.prototype.display = function() {
+  this.ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  this.cells.forEach((cell) => cell.display(this.ctx, CELL_SIZE));
+};
+
+const sim = new Simulation('sim');
+sim.display();
